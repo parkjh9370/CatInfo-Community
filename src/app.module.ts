619e53b25e97ae356077757898681c9685da658e
Jaehyeon <parkjh9370@naver.com>
2022-06-22 14:11:40 +0900

    init setting

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+    }),
+    CatsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
+
+/**
+ * 의존성 주입
+ * AppController : 소비자
+ * AppService: 제품
+ * 공급자가 제공해주는 제품을 소비자가 사용하고 있음
+ * Provider: 공급자
+ */
---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+import { PositiveIntPipe } from 'src/common/pipes/positiveInt.pipe';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+// 요청 수행 후 보내주는 응답 형태
+@UseInterceptors(SuccessInterceptor)
+// 에러 처리 형태
+@UseFilters(HttpExceptionFilter)
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCat() {
+    // 에러처리
+    // throw new HttpException('api is broken', 401);
+    return { cats: 'get all cat api' };
+  }
+
+  @Get(':id')
+  getOneCat(@Param('id', ParseIntPipe, PositiveIntPipe) param: number) {
+    // console.log(param);
+    return 'one cat';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return 'update partial cat';
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete cat';
+  }
+}
