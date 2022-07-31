d9ddc77f6dbefb41281c807da8df2399ff27a6af
Jaehyeon <parkjh9370@naver.com>
2022-07-31 10:33:20 +0900

    explain exception fillter

--- src/common/exceptions/http-exception.filter.ts
+++ src/common/exceptions/http-exception.filter.ts
@@ -9,14 +9,20 @@ import { Request, Response } from 'express';
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
+    // context 실행환경에 대해 응답, 요청 들고옴
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
+    // error type : string, object
+    // string : 직접 작성한 에러 메세지
+    // object : Nest 자체적인 에러 메세지(json 형태)
     const error = exception.getResponse() as
       | string
       | { error: string; statusCode: number; message: string | string[] };
 
+    // 응답값 형태
+    // 직접 인자값에 넣은 에러 (라우터 자체적인 에러)
     if (typeof error === 'string') {
       response.status(status).json({
         success: false,
